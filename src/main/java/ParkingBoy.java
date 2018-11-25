cec358541d2b6e3f9015f0d050ae6c5625fe4665
Iker Suen <suenik@oocl.com>
2018-11-25 11:38:31 +0800

    (iker) Story1 AC3 Part2 Finished

--- src/main/java/ParkingBoy.java
+++ src/main/java/ParkingBoy.java
@@ -8,10 +8,16 @@ public class ParkingBoy {
 
     public ParkingTicket park(Car car){
         ParkingTicket parkingTicket = new ParkingTicket(car);
-        return  parkingTicket;
+        this.lastErrorMessage = null;
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket){
+        if (parkingTicket == null){
+            this.lastErrorMessage = "Please provide your parking ticket.";
+            return null;
+        }
+
         if ( (parkingTicket.getCar() == null)){
             this.lastErrorMessage = "Unrecognized parking ticket.";
             return null;
--- src/test/java/ParkingLotsTest.java
+++ src/test/java/ParkingLotsTest.java
@@ -53,14 +53,58 @@ public class ParkingLotsTest {
     //Story1 AC3
     @Test
     void should_query_message_once_the_ticket_is_wrong() {
+        //Given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         ParkingTicket wrongTicket = new ParkingTicket();
-
+        //When
         parkingBoy.fetch(wrongTicket);
         String message = parkingBoy.getLastErrorMessage();
-
+        //Then
         assertEquals("Unrecognized parking ticket.", message);
     }
 
+    //Story1 AC3
+    @Test
+    void should_clear_the_message_once_the_operation_is_succeeded() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        ParkingTicket wrongTicket = new ParkingTicket();
+        //When
+        parkingBoy.fetch(wrongTicket);
+        assertNotNull(parkingBoy.getLastErrorMessage());
+        //Then
+        ParkingTicket ticket = parkingBoy.park(new Car());
+        assertNotNull(ticket);
+        assertNull(parkingBoy.getLastErrorMessage());
+    }
+
+    //Story1 AC3
+    @Test
+    void should_not_fetch_any_car_once_ticket_is_not_provided() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        //When
+        ParkingTicket ticket = parkingBoy.park(car);
+        //Then
+        assertNull(parkingBoy.fetch(null));
+        assertSame(car, parkingBoy.fetch(ticket));
+    }
+
+    //Story1 AC3
+    @Test
+    void should_query_message_once_ticket_is_not_provided() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        //When
+        parkingBoy.fetch(null);
+        //Then
+        assertEquals(
+                "Please provide your parking ticket.",
+                parkingBoy.getLastErrorMessage());
+    }
 }
