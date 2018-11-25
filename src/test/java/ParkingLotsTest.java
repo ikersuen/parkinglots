020f17f0333dbff15edd816776cfcf8f021d0cd2
Iker Suen <suenik@oocl.com>
2018-11-25 10:59:13 +0800

    (iker) Story1 AC2 Finished

--- src/test/java/ParkingLotsTest.java
+++ src/test/java/ParkingLotsTest.java
@@ -4,8 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotsTest {
     @Test
-    //AC1
-
+    //Story1 AC1
     void should_parking_boy_park_a_car_and_get_back_the_same_car() {
         //Given
         ParkingLot parkingLot = new ParkingLot();
@@ -16,6 +15,24 @@ public class ParkingLotsTest {
         Car fetched = parkingBoy.fetch(ticket);
         //Then
         assertSame(fetched, car);
+    }
+
+    @Test
+    //Story1 AC2
+    void should_parking_boy_park_multiple_cars(){
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        //When
+        ParkingTicket car1Ticket = parkingBoy.park(car1);
+        ParkingTicket car2Ticket = parkingBoy.park(car2);
+        Car fetchedCar1Ticket = parkingBoy.fetch(car1Ticket);
+        Car fetchedCar2Ticket = parkingBoy.fetch(car2Ticket);
+        //Then
+        assertSame(car1, fetchedCar1Ticket);
+        assertSame(car2, fetchedCar2Ticket);
     }
 
 }
